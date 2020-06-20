de6a34daadddc885d7df0990739d1ceea0b372c5
unknown <hmuley22@gmail.com>
2020-06-20 16:29:00 +0530

    Add the html file rendering in the routings

--- routings/admin.js
+++ routings/admin.js
@@ -1,17 +1,18 @@
 const express = require('express');
-
+const path = require('path')
+const rootDir = require('../helper/path');
 const router = express.Router();
 
 // You can keep both the urls same as both the middleware are of different requests!!
 
-router.get('/new-profile', (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
     // console.log("New Porfile is Adding...")
     // res.send('<h1> Add New Profile</h>')
-    res.send('<form action="/admin/profile" method="POST"><input type="text" name="title"><button type="submit">Add Product</button>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-//This middleware always access whether it is POST or GET request {here is the new logic mentioned for too restrict it to just POST request}, for that just change the app.use to app.post
-router.post('/profile', (req, res, next) => {
+//This middleware (app.use) always access whether it is POST or GET request {here is the new logic mentioned for too restrict it to just POST request}, for that just change the app.use to app.post
+router.post('/add-product', (req, res, next) => {
     console.log("This is the request body: ", req.body);
     res.redirect('/');
 });
--- routings/shop.js
+++ routings/shop.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { route } = require('./admin');
+const path = require('path')
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.get('/', (req, res, next) => {
     // const url = req.url;
     // console.log(url)
     // console.log("This is another middleware function");
-    res.send('<h2> Hello from the express js</h2>')
-})
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+});
 
 module.exports = router;
