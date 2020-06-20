56df5ee45159c649ae67dc7a7b2068d4dc3da2da
unknown <hmuley22@gmail.com>
2020-06-20 11:31:11 +0530

    New Routings in Express

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("This is a console statement");
+const express = require('express');
+
+const app = express();
+app.use('/profile', (req, res, next) => {
+    res.send('<h1> Add New Profile</h>')
+})
+
+app.use('/', (req, res, next) => {
+    const url = req.url;
+    console.log(url)
+    console.log("This is another middleware function");
+    res.send('<h2> Hello from the express js</h2>')
+})
+
+app.listen(8080);
