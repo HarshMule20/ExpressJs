27882c48ac0d74e5e7aba8a33827dabfe187bf76
unknown <hmuley22@gmail.com>
2020-06-28 02:19:37 +0530

    bugs fixing in the error file

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.not_found_products = (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path : '/404'});
   };
