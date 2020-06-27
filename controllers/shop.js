247ce38bdb47c444414a4a00cd178be495beb819
unknown <hmuley22@gmail.com>
2020-06-28 02:20:56 +0530

    add the new controller for admin and for shop

---
+++ controllers/shop.js
@@ -0,0 +1,39 @@
+const Product = require('../models/product');
+const router = require('../routes/shop');
+
+exports.getproducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product_list', {
+      prods: products,
+      pageTitle: 'All productsShop',
+      path: '/products',
+      // hasProducts: products.length > 0,
+      // activeShop: true,
+      // productCSS: true
+    });
+  });
+};
+
+exports.getindex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path : '/cart',
+    pageTitle : 'Your Cart'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    pageTitle : 'Checkout',
+    path : 'checkout'
+  });
+}
