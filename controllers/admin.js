d1690366daceaa986e1027e7d3dbb1fdc9cbd132
Harsh Mule <harshmule@apples-MacBook-Pro.local>
2020-07-09 18:10:48 +0530

    changes in the controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,72 +1,71 @@
 const Product = require('../models/product');
 
-
-exports.getaddproducts = (req, res, next) => {
-    res.render('admin/edit-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      editing: false,
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
-    });
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  });
 };
 
-exports.postaddproducts = (req, res, next) => {
-  // products.push({ title: req.body.title });
-//   const product = new Product(req.body.title);
-const title = req.body.title;
-const imageUrl = req.body.imageUrl;
-const description = req.body.description;
-const price = req.body.price;
-const product = new Product(null, title, imageUrl, description, price);
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
-  return res.redirect('/');
+  res.redirect('/');
 };
 
-exports.geteditproducts = (req, res, next) => {
-  const editmode = req.query.edit;
-  if(!editmode) {
-    res.redirect('/');
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
   }
-  const prodID = req.params.productId;
-  Product.findbyid(prodID, product => {
-    if(!product) {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
-      pageTitle: 'Add Product',
+      pageTitle: 'Edit Product',
       path: '/admin/edit-product',
-      editing: editmode,
+      editing: editMode,
       product: product
     });
   });
-  
 };
 
-exports.posteditproduct = (req, res, next) => {
-  const prod_id = req.body.productId;
-  const updatedtitle = req.body.title;
-  const updatedprice = req.body.price;
-  const updatedimageUrl = req.body.imageUrl;
-  const updateddescription =req.body.description;
-  const updated_product = new Product(prod_id, updatedtitle, updatedimageUrl, updateddescription, updatedprice);
-  updated_product.save();
-  return res.redirect('/admin/products');
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('admin/products', {
-          prods: products,
-          pageTitle: 'Admin Products',
-          path: '/admin/products'
-        });
-      });
-}
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  });
+};
 
-exports.postdeleteproduct = (req, res, next) => {
+exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteByid(prodId);
-  res.redirect('/admin/products'); 
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
